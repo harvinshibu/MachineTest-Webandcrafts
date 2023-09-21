e71a934b0b2fdc1d25a498a0a7b9873dc575b039
Harvin Shibu <harvinshibu47@gmail.com>
2023-09-21 14:51:13 +0530

    second commit

--- MachineTest-Webandcrafts/ContentView.swift
+++ MachineTest-Webandcrafts/ContentView.swift
@@ -261,18 +261,6 @@ struct ProductsView: View {
     }
 }
 
-struct Triangle: Shape {
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-        path.move(to: CGPoint(x: rect.maxX, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.maxX + 10, y: rect.midY))
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-        path.closeSubpath()
-
-        return path
-    }
-}
-
 
 struct CategoriesView: View {
     let text: String
